4458cc7133927d46df2ab7f52a3f86bc011e1d10
lsoaress <lucassoaresdecarvalho2013@gmail.com>
2022-08-27 02:52:26 -0300

    Correção na estilização da seção sobre, início da virada de card

---
+++ site/public/js/main.js
@@ -0,0 +1,33 @@
+function turn_around(n) {
+    switch (n) {
+        case 1:
+            foto1.style.backgroundColor = "red";
+
+            break;
+        case 2:
+            foto2.style.backgroundColor = "red";
+            break;
+        case 3:
+            foto3.style.backgroundColor = "red";
+            break;
+        case 4:
+            foto4.style.backgroundColor = "red";
+            break;
+        case 5:
+            foto5.style.backgroundColor = "red";
+            break;
+        case 6:
+            foto6.style.backgroundColor = "red";
+            break;
+    }
+}
+
+/* div_pai.onclick = function(){
+    localizar_setor(qtdSetor[c].idSetor)
+  }; */
+
+/*   var Foo = function(){
+    document.getElementById( "a" ).setAttribute( "onClick", "javascript: Boo();" );
+} */
+
+/* document.getElementById("a").onclick = function () { Foo(param); }; */

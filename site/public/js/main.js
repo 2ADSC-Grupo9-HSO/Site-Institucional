0e981801e323ba949a902fa0eacf5d5bb6f54a33
RafaelSampaio777 <rafael.macedo@sptech.school>
2022-08-27 17:40:29 -0300

    carrossel funcional

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -22,6 +22,37 @@ function turn_around(n) {
     }
 }
 
+const slides = document.querySelectorAll('[data-js="slide_item"]');
+const botaoProximo = document.querySelector('[data-js="slide_button-next"]');
+const botaoAnterior = document.querySelector('[data-js="slide_button-prev"]');
+
+let slideAtual = 0;
+const ultimoSlide = slides.length - 1;
+
+const manipuladorDeClasses = slideCorreto => {
+    slides.forEach(slide => {
+        slide.classList.remove('slide_item-visible');
+    });
+
+    slides[slideCorreto].classList.add('slide_item-visible');
+}
+
+botaoProximo.addEventListener('click', () => {
+    const slideCorreto = slideAtual === ultimoSlide
+    ? slideAtual = 0
+    : ++slideAtual
+
+    manipuladorDeClasses(slideCorreto);
+})
+
+botaoAnterior.addEventListener('click', () => {
+    const slideCorreto = slideAtual === 0
+    ? slideAtual = ultimoSlide
+    : --slideAtual
+
+    manipuladorDeClasses(slideCorreto);
+})
+
 /* div_pai.onclick = function(){
     localizar_setor(qtdSetor[c].idSetor)
   }; */

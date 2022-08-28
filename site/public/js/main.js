debd3b7196fdd965c6ff7b162a42cc603603dca0
lsoaress <lucassoaresdecarvalho2013@gmail.com>
2022-08-28 00:36:29 -0300

    flip das fotos na seção sobre feito, trocar foto

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -1,30 +1,3 @@
-
-
-
-function turn_around(n) {
-    switch (n) {
-        case 1:
-            foto1.style.backgroundColor = "red";
-
-            break;
-        case 2:
-            foto2.style.backgroundColor = "red";
-            break;
-        case 3:
-            foto3.style.backgroundColor = "red";
-            break;
-        case 4:
-            foto4.style.backgroundColor = "red";
-            break;
-        case 5:
-            foto5.style.backgroundColor = "red";
-            break;
-        case 6:
-            foto6.style.backgroundColor = "red";
-            break;
-    }
-}
-
 const slides = document.querySelectorAll('[data-js="slide_item"]');
 const botaoProximo = document.querySelector('[data-js="slide_button-next"]');
 const botaoAnterior = document.querySelector('[data-js="slide_button-prev"]');
@@ -55,13 +28,3 @@ botaoAnterior.addEventListener('click', () => {
 
     manipuladorDeClasses(slideCorreto);
 })
-
-/* div_pai.onclick = function(){
-    localizar_setor(qtdSetor[c].idSetor)
-  }; */
-
-/*   var Foo = function(){
-    document.getElementById( "a" ).setAttribute( "onClick", "javascript: Boo();" );
-} */
-
-/* document.getElementById("a").onclick = function () { Foo(param); }; */
